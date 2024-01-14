1ef1665d1396232a942c6fec28e8a75217078f70
Koda <85606515+koda-git@users.noreply.github.com>
2024-01-13 22:52:12 -0500

    attempted fix for default location

--- lib/place_details.dart
+++ lib/place_details.dart
@@ -163,7 +163,7 @@ class _Map extends StatelessWidget {
   final Set<Marker> markers;
 
   const _Map({
-    required this.center,
+    this.center = const LatLng(43.26150015783331, -79.91967923951988),
     required this.mapController,
     required this.onMapCreated,
     required this.markers,
@@ -180,7 +180,7 @@ class _Map extends StatelessWidget {
         child: GoogleMap(
           onMapCreated: onMapCreated,
           initialCameraPosition: const CameraPosition(
-            target: LatLng(43.26150015783331, -79.91967923951988),
+            target: center,
             zoom: 16,
           ),
           markers: markers,
