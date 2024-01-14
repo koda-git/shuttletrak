6becd4c07f4a33030dcd14068f8aa636799032d2
Koda <85606515+koda-git@users.noreply.github.com>
2024-01-13 19:40:21 -0500

    Update runFetcher.py

--- runFetcher.py
+++ runFetcher.py
@@ -5,12 +5,13 @@ import os
 # Define the paths for the scripts
 current_directory = os.path.dirname(os.path.realpath(__file__))
 request_reports_path = os.path.join(current_directory, '..', 'FindMy', 'request_reports.py')
-server_path = os.path.join(current_directory, 'server.py')
+poster_path = os.path.join(current_directory, '..', 'FindMy', 'poster.py')
 
 # Log file path
 log_file = os.path.join(current_directory, 'processes.log')
+# Creates a log file at directory.
 
-with open(log_file, 'a') as log:
+with open(log_file, 'a') as log: # a for append w for write mode, resets when restarted
     while True:
         # Run request_reports.py and log output
         print("Running request_reports.py...")
